3a686ee300bb61be9a6257d7414f911e498904d5
venanciogomani <venanciogomani@gmail.com>
2024-07-19 00:45:44 +0200

    get year for footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-
+  getCurrentYear() {
+    return new Date().getFullYear();
+  }
 }
